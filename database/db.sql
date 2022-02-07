19c5310cf6e98977f29b2967f0d9ed6809b61bdb
Alejandro Rafael Chavez <juanrafael020@gmail.com>
2022-02-07 01:07:28 -0500

    Dockerizando el servicio de base de datos

---
+++ database/db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS authors
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS pull_requests
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    author_id INT NOT NULL,
+    source_branch VARCHAR(250) NOT NULL,
+    target_branch VARCHAR(250) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_author
+        FOREIGN KEY (author_id)
+            REFERENCES authors(id)
+)
